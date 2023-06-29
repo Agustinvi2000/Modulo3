53f9c92b1a868af30d9444505b083591022d5360
Agustinvi2000 <agustinvi2000@hotmail.com>
2023-06-29 19:56:05 -0300

    correccion de direcciones

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    url: "http://localhost:8080/",
-    authURL: 'http://localhost:8080/auth/',
+    //url: "http://localhost:8080/",
+    //authURL: 'http://localhost:8080/auth/',
     // changePasswordURL: 'http://localhost:8080/email-password/'
-    //  url: "https://backend-portfolio-av.onrender.com/",
-    //  authURL: 'https://backend-portfolio-av.onrender.com/auth/',
+      url: "https://backend-portfolio-av.onrender.com/",
+      authURL: 'https://backend-portfolio-av.onrender.com/auth/',
     //  changePasswordURL: 'https://backend-portfolio-av.onrender.com/email-password/'
 };
